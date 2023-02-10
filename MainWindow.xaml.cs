324c57ac3aca79c6831080e134608ae665834c83
Balint837 <pecsi.balint@students.jedlik.eu>
2023-02-10 08:10:04 +0100

    asd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Reflection.Emit;
+using System.Threading;
 
 namespace BlackJack
 {
@@ -20,26 +23,193 @@ namespace BlackJack
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Image[,] cards { get; private protected set; } = new Image[2, 9];
+        public Random generator = new();
+        public bool MayPull = false;
+        public int HumanSum
+        {
+            get
+            {
+                int temp = 0;
+                for (int i = 1; i < PlayerHumanCards.Children.Count; i++)
+                {
+                    temp += (int)((Image)PlayerHumanCards.Children[i]).Tag;
+                }
+                return temp;
+            }
+        }
+
+        public int BotSum
+        {
+            get
+            {
+                int temp = 0;
+                for (int i = 1; i < PlayerBotCards.Children.Count; i++)
+                {
+                    temp += (int)((Image)PlayerBotCards.Children[i]).Tag;
+                }
+                return temp;
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
-            for (int playerIdx = 0; playerIdx < 2; playerIdx++)
+            for (int Idx = 0; Idx < 2; Idx++)
             {
-                for (int Idx = 0; Idx < 2; Idx++)
+                BitmapImage bitmapImg = new();
+                bitmapImg.BeginInit();
+                var randint = generator.Next(1, 52);
+                bitmapImg.UriSource = new Uri(System.IO.Path.Combine(Environment.CurrentDirectory, "img", $"69.gif"));
+                bitmapImg.DecodePixelWidth = 700;
+                bitmapImg.EndInit();
+                PlayerBotCards.Children.Add(new Image
+                {
+                    Source = bitmapImg,
+                    Tag = GetCardValue(randint)
+                });
+            }
+            for (int Idx = 0; Idx < 2; Idx++)
+            {
+                BitmapImage bitmapImg = new();
+                bitmapImg.BeginInit();
+                var randint = generator.Next(1, 52);
+                bitmapImg.UriSource = new Uri(System.IO.Path.Combine(Environment.CurrentDirectory, "img", $"{randint}.gif"));
+                bitmapImg.DecodePixelWidth = 700;
+                bitmapImg.EndInit();
+
+                PlayerHumanCards.Children.Add(new Image
+                {
+                    Source = bitmapImg,
+                    Tag = GetCardValue(randint)
+                });
+            }
+            {
+                BitmapImage bitmapImg = new();
+                bitmapImg.BeginInit();
+                bitmapImg.UriSource = new Uri(System.IO.Path.Combine(Environment.CurrentDirectory, "img/69.gif"));
+                bitmapImg.DecodePixelWidth = 700;
+                bitmapImg.EndInit();
+                PullCardImage.Source = bitmapImg;
+                PullCardImage.Tag = -1;
+                
+            }
+            Update_CardSums();
+            MayPull = true;
+        }
+
+        private void Button_PullCard(object sender, RoutedEventArgs e)
+        {
+            if (MayPull)
+            {
                 {
                     BitmapImage bitmapImg = new();
                     bitmapImg.BeginInit();
-                    bitmapImg.UriSource = new Uri("img/69.gif");
+                    var randint = generator.Next(1, 52);
+                    bitmapImg.UriSource = new Uri(System.IO.Path.Combine(Environment.CurrentDirectory, "img", $"{randint}.gif"));
                     bitmapImg.DecodePixelWidth = 700;
                     bitmapImg.EndInit();
-                    cards[playerIdx, Idx] = new Image
+                    PlayerHumanCards.Children.Add(new Image
                     {
-                        Source = bitmapImg
-                    };
+                        Source = bitmapImg,
+                        Tag = GetCardValue(randint)
+                    });
                 }
+                Update_CardSums();
+                Button_EndTurn(null, new());
             }
-                
+        }
+
+        private void Button_EndTurn(object sender, RoutedEventArgs e)
+        {
+            if (MayPull)
+            {
+                if (BotSum <= 16)
+                {
+                    BitmapImage bitmapImg = new();
+                    bitmapImg.BeginInit();
+                    var randint = generator.Next(1, 52);
+                    bitmapImg.UriSource = new Uri(System.IO.Path.Combine(Environment.CurrentDirectory, "img", $"69.gif"));
+                    bitmapImg.DecodePixelWidth = 700;
+                    bitmapImg.EndInit();
+                    PlayerBotCards.Children.Add(new Image
+                    {
+                        Source = bitmapImg,
+                        Tag = GetCardValue(randint)
+                    });
+                }
+                Update_CardSums();
+            }
+        }
+
+        private void Button_EndGame(object sender, RoutedEventArgs e)
+        {
+            if (MayPull)
+            {
+                MayPull = false;
+                PlayerHumanSum.Content = $"{HumanSum}";
+                PlayerBotSum.Content = $"{BotSum}";
+
+                if (HumanSum > BotSum)
+                {
+                    PlayerHumanSum.Content += ": Nyertél!";
+                    return;
+                }
+                if (HumanSum == BotSum)
+                {
+                    PlayerHumanSum.Content += ": Döntetlen!";
+                    PlayerBotSum.Content += ": Döntetlen!";
+                    return;
+                }
+                PlayerBotSum.Content += ": Vesztettél!";
+            }
+        }
+
+        private void Update_CardSums()
+        {
+            PlayerHumanSum.Content = $"{HumanSum}";
+
+            if (HumanSum > 21)
+            {
+                PlayerHumanSum.Content = $"{HumanSum}: Vesztettél!";
+                MayPull = false;
+                return;
+            }
+            if (HumanSum == 21)
+            {
+                PlayerHumanSum.Content = $"{HumanSum}: Nyertél!";
+                MayPull = false;
+                return;
+            }
+            
+
+            PlayerBotSum.Content = $"{BotSum}";
+
+            if (BotSum > 21)
+            {
+                PlayerHumanSum.Content = $"{HumanSum}: Nyertél!";
+                MayPull = false;
+                return;
+            }
+            if (BotSum == 21)
+            {
+                PlayerHumanSum.Content = $"{HumanSum}: Vesztettél!";
+                MayPull = false;
+                return;
+            }
+            
+        }
+
+        private int GetCardValue(int card)
+        {
+            int n = (card-1) % 13;
+            if (n == 0)
+            {
+                return 11;
+            }
+            if (n > 9)
+            {
+                return 10;
+            }
+            return n + 1;
         }
     }
 }
